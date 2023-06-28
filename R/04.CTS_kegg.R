b8c4d172b3bb9589dc3b14ab468f8fcbab0e359e
ShawnWx2019 <shawnwang2016@126.com>
2023-06-28 08:29:58 +0800

    fix cbf and cts

--- R/04.CTS_kegg.R
+++ R/04.CTS_kegg.R
@@ -6,9 +6,10 @@
 #' @description Get kegg cid by CTS
 #' @author Shawn Wang <http://www.shawnlearnbioinfo.top>.
 #' \email{shawnwang2016@@126.com}
-#' @param query 'character' or 'data.frame'; For single InChiKey, query equals the InChiKey, For multiple InChiKeys, query is a data frame from mda_pubchem_crawler.
+#' @param query 'character' or 'data.frame'; For single InChiKey, query equals the InChiKey, For multiple InChiKeys, query is a single column of input, InChIKey or name.
 #' @param type "single','multiple';  For one compound or multiple compounds.
 #' @param core_num 'numeric'; how many cores you want to use
+#' @param key 'character'; InChIKey or name?
 #' @return KEGGID InChikey 2 kegg cid.
 #' @importFrom magrittr %>%
 #' @importFrom stringr str_split str_extract
@@ -19,34 +20,49 @@
 #' @importFrom crayon green bold italic red yellow
 #' @export
 
-mda_CTS_kegg = function(query,type = "multiple",core_num = 8) {
+mda_CTS_kegg = function(query,type = "multiple",key = "InChIKey",core_num = 8) {
   #> message setting
   msg_yes = green$bold$italic;
   msg_no = red$bold$italic;
   msg_warning = yellow$bold$italic;
-  message(msg_yes("Start analysis... Get KEGG annotation via InChIKey. "))
-  CTS_url = "https://cts.fiehnlab.ucdavis.edu/rest/convert/InChIKey/KEGG/"
+  message(msg_yes("Start analysis... \nGet KEGG annotation. "))
+  if(key == "InChIKey") {
+    CTS_url = "https://cts.fiehnlab.ucdavis.edu/rest/convert/InChIKey/KEGG/"
+  } else if (key == "name") {
+    CTS_url = "https://cts.fiehnlab.ucdavis.edu/rest/convert/Chemical%20Name/KEGG/"
+  }
   ## get kegg via InChikey
   InChikey2kegg = function(url){
     a = read_html(url) %>%
       html_nodes("p") %>% html_text() %>%
-      gsub(pattern = '\"',replacement = "",x = .) %>%
-      str_split(.,pattern = ",",n = 4,simplify = T) %>%
-      as.data.frame() %>%
-      mutate(
-        InChIkey = gsub("searchTerm:","",V3),
-        KEGG = str_extract(V4,pattern = "C\\d+")
-      ) %>%
-      select(InChIkey,KEGG)
-    return(a)
+      gsub(pattern = '\"',replacement = "",x = .)
+    b = data.frame(
+      InChIKey = str_extract(string = a,"(?<=searchTerm\\:).*(?=,results\\:)"),
+      KEGG = str_extract(string = a,"(?<=results:\\[).*(?=\\]\\})")
+    )
+    return(b)
   }
-  InChikey2kegg_run = function(para_url) {
+  name2kegg <- function(url){
+      a = read_html(url) %>%
+        html_nodes("p") %>% html_text() %>%
+        gsub(pattern = '\"',replacement = "",x = .)
+      b = data.frame(
+        Compound_name = str_extract(string = a,"(?<=searchTerm\\:).*(?=,results\\:)"),
+        KEGG = str_extract(string = a,"(?<=results:\\[).*(?=\\]\\})")
+      )
+    return(b)
+  }
+  xx2kegg_run = function(para_url) {
     p <- progressr::progressor(steps = length(para_url));
     b = furrr::future_map_dfr(.x = para_url,.f = function(.x) {
       Sys.sleep(0.5)
       p()
       a <- tryCatch({
-        InChikey2kegg(url = .x)
+        if(key == "InChIKey") {
+          InChikey2kegg(url = .x)
+        } else if (key == "name") {
+          name2kegg(url = .x)
+        }
       },error = function(e) {
         message(msg_no('Failed'))
       })
@@ -54,20 +70,22 @@ mda_CTS_kegg = function(query,type = "multiple",core_num = 8) {
     })
     return(b)
   }
+
   if (type == "multiple") {
-    Inchikey = query %>%
-      mutate(url = paste0(CTS_url,InChIKey)) %>%
-      filter(InChIKey != "NA") %>% 
+    input = query %>% setNames("tag") %>% 
+      mutate(tag = URLencode(tag,reserved = TRUE,repeated = T)) %>% 
+      mutate(url = paste0(CTS_url,tag)) %>%
+      filter(tag != "NA") %>% 
       select(url) %>% unique()
   } else if (type == "single"){
-    Inchikey = data.frame(
-      url = paste0(url,query)
+    input = data.frame(
+      url = paste0(CTS_url,query)
     )
   }
-  Inchikey = Inchikey$url
+  input.url = input$url
   future::plan("multisession",workers = core_num)
   with_progress({
-    KEGGID = InChikey2kegg_run(para_url = Inchikey)
+    KEGGID = xx2kegg_run(para_url = input.url)
   }
   )
   message(msg_yes("Done!"))
